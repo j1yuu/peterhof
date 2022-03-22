38a7f0eea262902505f2fadd57a49792b5b60ce3
j1yuu <59069015+j1yuu@users.noreply.github.com>
2022-03-23 02:21:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -61,3 +61,29 @@ const mySwap = () => {
 };
 
 mySwap();
+
+const myScroll = () => {
+    const links = document.querySelectorAll('.header__link');
+
+    const newArray = [...links];
+
+    newArray.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault()
+
+            const id = element.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+
+            if (section) {
+                section.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                console.log('Такое не имею')
+            }
+        })
+    })
+};
+
+myScroll()
